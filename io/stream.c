8037619335b5e0ae46371a820a21812e135966c3
holyshared <holy.shared.design@gmail.com>
2016-08-30 13:09:20 +0900

    add stream

---
+++ io/stream.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) {
+  FILE *in, *out;
+
+  struct pirate {
+    char name[100];
+    unsigned long booty;
+    unsigned int bread_len;
+  };
+
+  struct pirate p;
+  struct pirate blackbread = { "Edward Teach", 950, 48 };
+
+  out = fopen("data", "w");
+  if (!out) {
+    perror("fopen");
+    return 1;
+  }
+
+  if (!fwrite(&blackbread, sizeof(struct pirate), 1, out)) {
+    perror("fwrite");
+    return 1;
+  }
+
+  if (fclose(out)) {
+    perror("fclose");
+    return 1;
+  }
+
+  in = fopen("data", "r");
+  if (!in) {
+    perror("fopen");
+    return 1;
+  }
+
+  if (!fread(&p, sizeof(struct pirate), 1, in)) {
+    perror("fread");
+    return 1;
+  }
+
+  if (fclose(in)) {
+    perror("fclose");
+    return 1;
+  }
+
+  printf("name=\"%s\" booty=\"%lu\" bread_len=\"%u\"\n", p.name, p.booty, p.bread_len);
+
+  return 0;
+}
