f01c1d9473b63e9d433aa1c82454464cdd100444
holyshared <holy.shared.design@gmail.com>
2018-03-27 17:38:00 +0900

    append fwrite/fread n size

--- io/stream_n.c
+++ io/stream_n.c
@@ -9,44 +9,85 @@ struct user {
 };
 
 ssize_t write_users(struct user *users, int size, FILE *out) {
-  ssize_t done_size;
   int total = size;
 
   while (total > 0) {
-    done_size = fwrite(&users, sizeof(struct user), 1, out);
+    ssize_t wrote_size = fwrite(users, sizeof(struct user), total, out);
 
-    printf("done = %zu\n", done_size);
-
-    if (done_size <= 0) {
+    if (wrote_size <= 0) {
       return -1;
     }
-    total--;
+
+    total -= wrote_size;
+    users += wrote_size;
   }
 
   return 0;
 }
 
+ssize_t read_users(struct user *users, int size, FILE *in) {
+  int total = size;
+
+  while (total > 0 && feof(in) == 0) {
+    ssize_t readed_size = fread(users, sizeof(struct user), total, in);
+
+    if (readed_size <= 0) {
+      return -1;
+    }
+
+    total -= readed_size;
+    users += readed_size;
+  }
+
+  return 0;
+}
+
+ssize_t write_all_users(struct user *users, int total) {
+  FILE *out = fopen("users", "w");
+  ssize_t ret = write_users(users, total, out);
+  fclose(out);
+  return ret;
+}
+
+ssize_t read_all_users(struct user *users, int total) {
+  FILE *in = fopen("users", "r");
+  ssize_t ret = read_users(users, total, in);
+  fclose(in);
+  return ret;
+}
+
 int main() {
-  FILE *out;
   ssize_t ret;
 
-  struct user users[] = {
+  struct user output_users[] = {
     { "User1", "password1" },
     { "User2", "password2" },
     { "User3", "password3" }
   };
-  int total = (sizeof(users) / sizeof(struct user));
 
-  out = fopen("users", "w");
-
-  ret = write_users(&users, total, out);
+  int total = (sizeof(output_users) / sizeof(struct user));
+  ret = write_all_users(output_users, total);
 
   if (ret == -1) {
     printf("output failed!!");
     return FAILED;
   }
 
-  fclose(out);
+  struct user input_users[3];
+  int read_size = (sizeof(input_users) / sizeof(struct user));
+
+  if (read_all_users(input_users, read_size) == -1) {
+    printf("output failed!!");
+    return FAILED;
+  }
+
+  int pos = 0;
+
+  while (read_size > pos) {
+    printf("name=%s\n", input_users[pos].name);
+    printf("password=%s\n", input_users[pos].password);
+    pos++;
+  }
 
   return SUCCESSFUL;
 }
