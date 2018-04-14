5efc31c3832defa2196d538c788169c62ccaed6b
holyshared <holy.shared.design@gmail.com>
2018-04-14 19:08:43 +0900

    add memfd_proc

--- process/memfd_proc.c
+++ process/memfd_proc.c
@@ -1,5 +1,33 @@
+#include <sys/syscall.h>
 #include <linux/memfd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+/**
+ * int memfd_create(const char *name, unsigned int flags);
+ */
+static int memfd_create(const char *name, unsigned int flags) {
+  return syscall(SYS_memfd_create, name, flags);
+}
 
 int main() {
-  return 0;
+  int fd = memfd_create("shared", MFD_ALLOW_SEALING);
+
+  if (fd == -1) {
+    printf("print error string by strerror: %s\n", strerror(errno));
+    return -1;
+  }
+
+  ssize_t len;
+
+  if (ftruncate(fd, len) == -1) {
+    printf("print error string by strerror: %s\n", strerror(errno));
+    return -1;
+  }
+
+  exit(EXIT_SUCCESS);
 }
